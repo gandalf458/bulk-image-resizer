176fd970e6e0288b524dc7e1a7b0120869e12b7f
gandalf458 <gandalf458@gmail.com>
2019-07-18 14:37:49 +0200

    Add files via upload
    Resizing algorithm updated.

--- imageResizer.php
+++ imageResizer.php
@@ -1,10 +1,14 @@
 <?php
 /*
- * A simple script to resize all jpeg and png images in a directory to a specified width 
+ * A script to resize all jpeg and png images in a directory to a specified width
  * or height.
  *
- * (c) Copyright 2014-18, Irwin Associates and Graham R Irwin - www.irwinassociates.eu
+ * (c) Copyright 2014-19, Irwin Associates and Graham R Irwin - www.irwinassociates.eu
+ *
  * Last updated:
+ * 18 Jul 2019 - @ out exif_read_data + changes to calcSize()
+ * 13 Nov 2018 - fixed a bug introduced with previous update; also converts saved file
+ *               name to lowercase; some cosmetic changes and code improvements
  * 14 Oct 2018 - rotate image if necessary
  *  4 Apr 2016 - minor changes; released on github
  *  9 Jun 2015 - minor correction
@@ -13,10 +17,10 @@
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal in the Software
  * without restriction, including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit
  * persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or 
+ * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
@@ -34,30 +38,30 @@
 <title>Irwin Associates image resizer</title>
 <style>
 body {
-	font: .83em sans-serif;
-	max-width: 60em;
+  font: .91em/1.3 sans-serif;
+  max-width: 50em;
 }
 ul {
-	padding-left: 1.5em;
+  padding-left: 1.5em;
 }
 label {
-	float: left;
-	width: 8em;
+  float: left;
+  width: 8em;
 }
 input {
-	display: block;
-	font: inherit;
-	margin-bottom: .5em;
+  font: inherit;
+  margin-bottom: .5em;
 }
 input[type="text"],
 input[type="number"] {
-	border: solid 1px #a0a0a0;
-	box-shadow: 2px 2px 2px #c2c2c2;
-	padding: 2px 3px;
-	background: #f0fdff;
+  padding: 1px 3px;
 }
 input[type="number"] {
-	width: 5em;
+  width: 5em;
+}
+input[type="submit"] {
+  padding: 1px 12px;
+  cursor: pointer;
 }
 </style>
 </head>
@@ -66,59 +70,76 @@ input[type="number"] {
 <h2>Image Resizer</h2>
 <?php
 
+function calcSize($fw, $fh, $mw, $mh, $w, $h) {
+  // fixed width, fixed height, max width, max height, image width, image height
+  // nw = new width, nh = new height
+
+  if ($fw) {
+    $nw = $fw;
+    $nh = ($nw / $w) * $h;
+  }
+  elseif ($fh) {
+    $nh = $fh;
+    $nw = ($nh / $h) * $w;
+  }
+  elseif ($w < $h) {     // image is portrait
+    $nh = $mh;
+    $nw = ($nh / $h) * $w;
+    if ($nw > $mw) {
+      $nw = $mw;
+      $nh = ($nw / $w) * $h;
+    }
+  }
+  elseif ($w > $h) {     // image is landscape
+    $nw = $mw;
+    $nh = ($nw / $w) * $h;
+    if ($nh > $mh) {
+      $nh = $mh;
+      $nw = ($nh / $h) * $w;
+    }
+  }
+  else {                 // image is square
+    $nw = $mh;
+    $nh = $mh;
+  }
+  return(array($nw, $nh));
+
+}
+
 function resizer($fileName, $maxWidth, $maxHeight, $fixedWidth, $fixedHeight, $oldDir, $newDir, $quality) {
 
   $file = $oldDir.'/'.$fileName;
-  $fileDest = $newDir.'/'.$fileName;
+  $fileDest = $newDir.'/'.strtolower($fileName);   // save with lowercase file name
   list($width, $height) = getimagesize($file);
 
-  if ( $fixedWidth ) {
-    $newWidth  = $fixedWidth;
-    $newHeight = ($newWidth / $width) * $height;
-
-  } elseif ( $fixedHeight ) {
-    $newHeight = $fixedHeight;
-    $newWidth  = ($newHeight / $height) * $width;
-
-  } elseif ( $width < $height ) {			// image is portrait
-    $newHeight = $maxHeight;
-    $newWidth  = ($newHeight / $height) * $width;
-
-  } elseif ( $width > $height ) {			// image is landscape
-    $newWidth  = $maxWidth;
-    $newHeight = ($newWidth / $width) * $height;
-
-  } else {								          	// image is square
-    $newWidth  = $maxHeight;
-    $newHeight = $maxHeight;
-  }
+  list($newWidth, $newHeight) = calcSize($fixedWidth, $fixedHeight, $maxWidth, $maxHeight, $width, $height);
 
   $extn = strtolower(pathinfo($file, PATHINFO_EXTENSION));
 
   // it's a jpeg
-  if ( $extn == 'jpg' or $extn == 'jpeg' ) {
-    $imageSrc = imagecreatefromjpeg($file);
+  if ($extn === 'jpg' || $extn === 'jpeg') {
+    $imageSrc  = imagecreatefromjpeg($file);
     // rotate image if necessary
-    $exif = exif_read_data($file);
-    if ( isset($exif['Orientation']) ) {
-      switch ( $exif['Orientation'] ) {
+    $exif = @exif_read_data($file);
+    if (isset($exif['Orientation'])) {
+      switch ($exif['Orientation']) {
         case 3:
           $imageSrc = imagerotate($imageSrc, 180, 0);
           break;
         case 6:
           $imageSrc = imagerotate($imageSrc, -90, 0);
-          list($height, $width) = array($width, $height);
-          list($newHeight, $newWidth) = array($newWidth, $newHeight);
+          list($height, $width) = array($width, $height);  // swap width and height
+          list($newWidth, $newHeight) = calcSize($fixedWidth, $fixedHeight, $maxWidth, $maxHeight, $width, $height);  // recalculate the size
           break;
         case 8:
           $imageSrc = imagerotate($imageSrc, 90, 0);
-          list($height, $width) = array($width, $height);
-          list($newHeight, $newWidth) = array($newWidth, $newHeight);
+          list($height, $width) = array($width, $height);  // swap width and height
+          list($newWidth, $newHeight) = calcSize($fixedWidth, $fixedHeight, $maxWidth, $maxHeight, $width, $height);  // recalculate the size
           break;
       }
     }
     $imageDest = imagecreatetruecolor($newWidth, $newHeight);
-    if ( imagecopyresampled($imageDest, $imageSrc, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height) ) {
+    if (imagecopyresampled($imageDest, $imageSrc, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height)) {
       imagejpeg($imageDest, $fileDest, $quality);
       imagedestroy($imageSrc);
       imagedestroy($imageDest);
@@ -128,12 +149,12 @@ function resizer($fileName, $maxWidth, $maxHeight, $fixedWidth, $fixedHeight, $o
   }
 
   // it's a png
-  if ( $extn == 'png' ) {
+  if ($extn === 'png') {
     $imageSrc  = imagecreatefrompng($file);
     $imageDest = imagecreatetruecolor($newWidth, $newHeight);
     imagealphablending($imageDest, false);
     imagesavealpha($imageDest, true);
-    if ( imagecopyresampled($imageDest, $imageSrc, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height) ) {
+    if (imagecopyresampled($imageDest, $imageSrc, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height)) {
       imagepng($imageDest, $fileDest, ($quality / 10) - 1);
       imagedestroy($imageSrc);
       imagedestroy($imageDest);
@@ -145,75 +166,87 @@ function resizer($fileName, $maxWidth, $maxHeight, $fixedWidth, $fixedHeight, $o
 }
 
 
-// set as needed (depending on the number and size of photos 60 should usually suffice)
+// set as needed depending on the number and size of photos (60 should usually suffice)
 set_time_limit(60);
 
-if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) :
-  $maxWidth    = $_POST['maxWidth'];
-  $maxHeight   = $_POST['maxHeight'];
-  $fixedWidth  = $_POST['fixedWidth'];
-  $fixedHeight = $_POST['fixedHeight'];
-  $oldDir      = $_POST['oldDir'];
-  $newDir      = $_POST['newDir'];
-  $quality     = $_POST['quality'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') :
+  $maxWidth    = (int)$_POST['maxWidth'];
+  $maxHeight   = (int)$_POST['maxHeight'];
+  $fixedWidth  = (int)$_POST['fixedWidth'];
+  $fixedHeight = (int)$_POST['fixedHeight'];
+  $oldDir      = htmlspecialchars($_POST['oldDir']);
+  $newDir      = htmlspecialchars($_POST['newDir']);
+  $quality     = (int)$_POST['quality'];
 
   // create destination directory if it doesn't exist
-  if ( !file_exists($newDir) )
+  if (!file_exists($newDir))
     mkdir($newDir);
-
-  // check source directory exists, open it and get all files
-  if ( !file_exists($oldDir) )
+  // check source directory exists
+  if (!file_exists($oldDir))
     die('Source directory does not exist.');
-  $folder = opendir($oldDir);
-  while ( $file = readdir($folder) ) {
-    if ( $file[0] != '.' && $file[0] != '..' )
-      $files[$file] = $file;
-  }
-  if ( ( $filess = @scandir($oldDir) ) && count($filess) <= 2 )
+  // get all files
+  $files = scandir($oldDir);
+  if (count($files) <= 2)
     die('Source directory is empty.');
 
   echo '<p>Settings: fixed width ', $fixedWidth, ', fixed height ', $fixedHeight, ', max width ', $maxWidth, ', max height ', $maxHeight, ', quality ', $quality, '%</p>', PHP_EOL;
   echo '<ul>', PHP_EOL;
-
   // process each file
-  foreach ( $files as $key => $value ) {
-    $ext = strtolower(pathinfo($key, PATHINFO_EXTENSION));
-    if ( $ext == 'jpg' or $ext == 'jpeg' or $ext == 'png' ) {
-      if ( resizer($key, $maxWidth, $maxHeight, $fixedWidth, $fixedHeight, $oldDir, $newDir, $quality) )
-        echo '<li>Resized image: ', $key, '</li>', PHP_EOL;
-      else
-        echo '<li>** Failed to resize image: ', $key, ' **</li>', PHP_EOL;
-    } else {
-      echo '<li>** ', $key, ' is not a jpeg or png **</li>', PHP_EOL;
+  foreach ($files as $file) {
+    if ($file !== '.' && $file !== '..') {
+      $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+      if ($ext === 'jpg' || $ext === 'jpeg' || $ext === 'png') {
+        if (resizer($file, $maxWidth, $maxHeight, $fixedWidth, $fixedHeight, $oldDir, $newDir, $quality)) {
+          echo '<li>Resized image: ', $file, '</li>', PHP_EOL;
+        } else {
+          echo '<li>** Failed to resize image: ', $file, ' **</li>', PHP_EOL;
+        }
+      } else {
+        echo '<li>** ', $file, ' is not a jpeg or png **</li>', PHP_EOL;
+      }
     }
   }
   echo '</ul>', PHP_EOL;
-
-  closedir($folder);
   echo '<p>*** Finished ***</p>', PHP_EOL;
   echo '<p><a href="', htmlspecialchars($_SERVER['PHP_SELF']), '">Resize more</a></p>', PHP_EOL;
 
 else :
 ?>
-<p>This script will resize all jpeg and png images in the source directory to a specified width or height. The source and destination directories will usually be subdirectories of the directory containing the script. The destination directory will be created if it doesn’t exist. You can specify the names of these directories as well as other parameters:</p>
+<p>This script will resize all jpeg and png images in the source directory to a specified fixed or maximum width or height. The source and destination directories should be subdirectories of the directory containing the script. The destination directory will be created if it doesn’t exist.</p>
 <form id="form1" name="form1" method="post">
-  <label for="maxWidth">Max width</label>
-  <input type="number" name="maxWidth" id="maxWidth" value="600">
-  <label for="maxHeight">Max height</label>
-  <input type="number" name="maxHeight" id="maxHeight" value="600" size="6">
-  <label for="fixedWidth">Fixed width</label>
-  <input type="number" name="fixedWidth" id="fixedWidth" value="0" size="6">
-  <label for="fixedHeight">Fixed height</label>
-  <input type="number" name="fixedHeight" id="fixedHeight" value="0" size="6">
-  <label for="oldDir">Source directory</label>
-  <input type="text" name="oldDir" id="oldDir" value="originals">
-  <label for="newDir">Dest’n directory</label>
-  <input type="text" name="newDir" id="newDir" value="images">
-  <label for="quality">Quality %</label>
-  <input type="number" name="quality" id="quality" min="10" max="100" value="80" size="6">
-  <input type="submit" name="submit" id="submit" value="Resize">
+  <div>
+    <label for="maxWidth">Max width</label>
+    <input type="number" name="maxWidth" id="maxWidth" value="600">
+  </div>
+  <div>
+    <label for="maxHeight">Max height</label>
+    <input type="number" name="maxHeight" id="maxHeight" value="600">
+  </div>
+  <div>
+    <label for="fixedWidth">Fixed width</label>
+    <input type="number" name="fixedWidth" id="fixedWidth" value="0">
+  </div>
+  <div>
+    <label for="fixedHeight">Fixed height</label>
+    <input type="number" name="fixedHeight" id="fixedHeight" value="0">
+  </div>
+  <div>
+    <label for="oldDir">Source directory</label>
+    <input type="text" name="oldDir" id="oldDir" value="originals">
+  </div>
+  <div>
+    <label for="newDir">Dest’n directory</label>
+    <input type="text" name="newDir" id="newDir" value="images">
+  </div>
+  <div>
+    <label for="quality">Quality %</label>
+    <input type="number" name="quality" id="quality" min="10" max="100" value="80">
+  </div>
+  <div>
+    <input type="submit" name="submit" id="submit" value="Resize">
+  </div>
 </form>
-<p>Once you have pressed Resize please be patient as it may take some minutes to resize your images.</p>
+<p>Once you have pressed Resize please be patient as it may take some minutes to resize your images depending on how many there are.</p>
 <?php
 endif;
 ?>
